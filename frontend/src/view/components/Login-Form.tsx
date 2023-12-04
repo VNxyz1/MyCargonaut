b66c9d944e64b37deede84b79d653835c5f10689
VNxyz1 <vincenzo_n@rocketmail.com>
2023-12-04 16:38:47 +0100

    login functional

--- frontend/src/view/components/Login-Form.tsx
+++ frontend/src/view/components/Login-Form.tsx
@@ -3,19 +3,60 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import {useState} from "react";
 
+interface LoginDataProps {
+    eMail: string;
+    password: string;
+}
+
 function LoginForm() {
-    const [validated, setValidated] = useState(false);
+    const [feedback, setFeedback] = useState<string | undefined>(undefined);
+    const [loginData, setLoginData] = useState<LoginDataProps>({eMail: "", password: ""});
 
-    const handleSubmit = (event: any) => {
-        const form = event.currentTarget;
-        event.preventDefault();
-
-        if (form.checkValidity() === false) {
+    const handleSubmit = async (event: any) => {
+            event.preventDefault();
             event.stopPropagation();
+            const res = await postLogin();
+            if (res) {
+                console.log(res.message);
+                setFeedback(res.message);
+
+                if (res.successful) {
+                    window.location.href = "/"
+                }
+            }
+
+    };
+
+
+    const postLogin = async () => {
+        try {
+            const response = await fetch("/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(loginData),
+            });
+            if (!response.ok) {
+                const data = await response.json();
+                return {successful: false, message: data.message}
+            } else {
+                const data = await response.json();
+                return {successful: true, message: data.message}
+            }
+        } catch (error) {
+            console.error("Error:", error);
         }
 
-        setValidated(true);
-    };
+    }
+
+
+    const handleFormChange = (prop: string, value: string) => {
+        setLoginData({
+            ...loginData,
+            [prop]: value
+        })
+    }
 
 
     return (
@@ -23,12 +64,27 @@ function LoginForm() {
             <Card style={{width: '30rem', height: 'min-content'}}>
                 <Card.Body>
                     <Card.Title>Login</Card.Title>
-                    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                    <Form noValidate onSubmit={handleSubmit}>
                         <Form.Group className="mb-3" controlId="loginEmail">
-                            <Form.Control required type="email" placeholder="E-Mail"/>
+                            <Form.Control
+                                type="email"
+                                placeholder="E-Mail"
+                                onChange={(event) =>handleFormChange("eMail", event.target.value)}
+                            />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="loginPassword">
-                            <Form.Control required type="password" placeholder="Passwort"/>
+                            <Form.Control
+                                type="password"
+                                placeholder="Passwort"
+                                onChange={(event) =>handleFormChange("password", event.target.value)}
+                            />
+                            {!feedback ?
+                                <></>
+                                :
+                                <p style={{fontStyle: "italic"}}>
+                                    {feedback}
+                                </p>
+                            }
                             <Form.Text className="text-muted formLink">
                                     Passwort vergessen?
                             </Form.Text>
