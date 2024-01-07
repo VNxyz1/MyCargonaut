9ca12d97602b67ac37b548dd0cf6ff3877949c81
VNxyz1 <vincenzo_n@rocketmail.com>
2024-01-07 23:30:12 +0100

    updated the api documentation

--- backend/src/routes/offer/offer.controller.ts
+++ backend/src/routes/offer/offer.controller.ts
@@ -13,7 +13,7 @@ import {
   Session,
   UseGuards,
 } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {ApiOperation, ApiQuery, ApiResponse, ApiTags} from '@nestjs/swagger';
 import { OfferService } from '../offer.service/offer.service';
 import { OKResponseWithMessageDTO } from '../../generalDTOs/OKResponseWithMessageDTO';
 import { CreateOfferDto } from './DTOs/CreateOfferDto';
@@ -78,9 +78,19 @@ export class OfferController {
   @Get('search')
   @ApiOperation({
     summary:
-      'gets offers, filtered by a string (searches in the description, plz, location)',
+      'Gets offers that match a search string, including the description, location (plz), and date.',
+    description: 'Example request: "offers/search?search=test&fromPLZ=63679&toPLZ=64002&rating=4&date=2025-01-01".',
   })
-  @ApiResponse({ type: GetAllOffersResponseDto })
+  @ApiQuery({ name: 'search', description: "A search string to filter offers by." })
+  @ApiQuery({ name: 'fromPLZ', description: "The starting postal code for filtering offers by location." })
+  @ApiQuery({ name: 'toPLZ', description: "The ending postal code for filtering offers by location." })
+  @ApiQuery({ name: 'seats', description: "A number of seats to filter offers by." })
+  @ApiQuery({ name: 'date', description: "A date to filter offers by. Must be in the format YYYY-MM-DD." })
+  @ApiQuery({ name: 'rating', description: "A rating to filter offers by. Must be between 0 and 5." })
+  @ApiResponse({
+    type: GetAllOffersResponseDto,
+    description: 'An array of offers that match the search criteria.',
+  })
   async getFilteredOffers(
     @Query('search') searchString?: string,
     @Query('fromPLZ') fromPLZ?: string,
