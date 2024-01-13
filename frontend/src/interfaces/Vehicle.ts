93cb9e07c41c2d812ca77eb26d580f48889c348f
Anastasia Kunst <anastasia.kunst@gmx.de>
2024-01-13 23:26:22 +0100

    placeholder data

---
+++ frontend/src/interfaces/Vehicle.ts
@@ -0,0 +1,8 @@
+export interface Vehicle {
+    id: number;
+    ownerId: number;
+    name: string;
+    vehiclePictureUrl: string;
+    seats: number;
+    description: string;
+}
--- frontend/src/view/components/Profile/MyVehiclesComponent.tsx
+++ frontend/src/view/components/Profile/MyVehiclesComponent.tsx
@@ -1,32 +1,63 @@
 import Container from 'react-bootstrap/Container';
-import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import { Vehicle } from "../../../interfaces/Vehicle";
+import placeholderImg from "../../../assets/img/platzhalter_auto.jpg";
+
+const exampleData: Vehicle[] = [
+    {
+        id: 1,
+        ownerId: 1,
+        name: "Porsche Cayenne",
+        vehiclePictureUrl: "",
+        seats: 3,
+        description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    },
+    {
+        id: 2,
+        ownerId: 1,
+        name: "Golf GTI",
+        vehiclePictureUrl: "",
+        seats: 3,
+        description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    },
+    {
+        id: 3,
+        ownerId: 1,
+        name: "Porsche GT3 RS",
+        vehiclePictureUrl: "",
+        seats: 1,
+        description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    },
+];
 
 function MyVehiclesComponent() {
+
     return (
         <Container>
-            <p>Du hast noch keine Fahrzeuge angelegt.</p>
-
-            <Accordion defaultActiveKey="0">
-
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>Accordion Item #1</Accordion.Header>
-                    <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
-                    </Accordion.Body>
-                </Accordion.Item>
-
-
-            </Accordion>
-
+            {exampleData.length === 0 ? (
+                <p>Du hast noch keine Fahrzeuge angelegt.</p>
+            ) : (
+                <div className="card-container">
+                    {exampleData.map(vehicle => (
+                        <Card className="vehicle-card">
+                            <img
+                                src={vehicle.vehiclePictureUrl ? `${window.location.protocol}//${window.location.host}/user/profile-image/${vehicle.vehiclePictureUrl}` : placeholderImg}
+                                alt="User profile image"
+                            />
+                            <Card.Body>
+                                <Card.Text>
+                                    <strong>Sitze:</strong> {vehicle.seats}
+                                </Card.Text>
+                                <Card.Text>
+                                    <strong>Beschreibung:</strong> {vehicle.description}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    ))}
+                </div>
+            )}
         </Container>
     );
 }
 
 export default MyVehiclesComponent;
-
