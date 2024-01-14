5b76dfdb1c5b670fe0c66e078ce1059bab50a416
Jonas Nickel <derdingen@web.de>
2024-01-14 14:03:41 +0100

    GET all unread messages for the logged in user

---
+++ backend/src/routes/message/DTOs/GetUnreadMessagesCountDTO.ts
@@ -0,0 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { GetUnreadMessageCountDto } from './GetUnreadMessageCountDTO';
+export class GetUnreadMessagesCountDto {
+    @ApiProperty()
+    totalUnreadMessages: number;
+
+    @ApiProperty({ type: GetUnreadMessageCountDto })
+    conversations: GetUnreadMessageCountDto[];
+}
