f563d0b46e7b56861a8cc591e9fa6cd32f93a39f
VNxyz1 <vincenzo_n@rocketmail.com>
2024-01-14 14:57:33 +0100

    GetOtherUserDto now returns an indicator if the user provided a phone number, the age, offers and tripRequests

--- backend/src/routes/request/request.controller.ts
+++ backend/src/routes/request/request.controller.ts
@@ -30,7 +30,7 @@ import { User } from '../../database/User';
 import { UserService } from '../user.service/user.service';
 import { GetAllTripRequestResponseDto } from './DTOs/GetAllTripRequestResponseDto';
 import { GetTripRequestResponseDto } from './DTOs/GetTripRequestResponseDto';
-import { convertUserToOtherUser } from '../utils/convertToOfferDto';
+import { convertTripRequestToGetDto, convertUserToOtherUser } from '../utils/convertToOfferDto';
 import { UpdateTripRequestRequestDto } from './DTOs/UpdateTripRequestRequestDto';
 import { existsSync, unlinkSync } from 'fs';
 import { fileInterceptor } from './requesterFileInterceptor';
@@ -87,7 +87,7 @@ export class RequestController {
     const tRArr = await this.requestService.getAll();
     const dto = new GetAllTripRequestResponseDto();
     dto.tripRequests = tRArr.map((tR) => {
-      return this.convertToGetDto(tR);
+      return convertTripRequestToGetDto(tR);
     });
     return dto;
   }
@@ -98,7 +98,7 @@ export class RequestController {
   async getOne(@Param('id', ParseIntPipe) tripRequestId: number) {
     const tR = await this.requestService.getById(tripRequestId);
 
-    return this.convertToGetDto(tR);
+    return convertTripRequestToGetDto(tR);
   }
 
   @Get('search')
@@ -146,7 +146,7 @@ export class RequestController {
 
     const dto = new GetAllTripRequestResponseDto();
     dto.tripRequests = tRArr.map((tR) => {
-      return this.convertToGetDto(tR);
+      return convertTripRequestToGetDto(tR);
     });
     return dto;
   }
@@ -457,7 +457,7 @@ export class RequestController {
     const dto = new GetOfferingDto();
     dto.id = offering.id;
     dto.offeringUser = convertUserToOtherUser(offering.offeringUser);
-    dto.tripRequest = this.convertToGetDto(offering.tripRequest);
+    dto.tripRequest = convertTripRequestToGetDto(offering.tripRequest);
     dto.text = offering.text;
     dto.requestedCoins = offering.requestedCoins;
     dto.accepted = offering.accepted;
@@ -547,21 +547,6 @@ export class RequestController {
     }
 
     unlinkSync(oldImagePath);
-  }
-
-  convertToGetDto(tripRequest: TripRequest) {
-    const dto = new GetTripRequestResponseDto();
-    dto.id = tripRequest.id;
-    dto.requester = convertUserToOtherUser(tripRequest.requester);
-    dto.startPlz = tripRequest.startPlz;
-    dto.endPlz = tripRequest.endPlz;
-    dto.createdAt = new Date(tripRequest.createdAt);
-    dto.description = tripRequest.description;
-    dto.cargoImg = tripRequest.cargoImg;
-    dto.startDate = tripRequest.startDate;
-    dto.seats = tripRequest.seats;
-
-    return dto;
   }
 
   filterRequestsBySeats(
--- backend/src/routes/utils/convertToOfferDto.ts
+++ backend/src/routes/utils/convertToOfferDto.ts
@@ -3,6 +3,9 @@ import { GetOfferResponseDto } from '../offer/DTOs/GetOfferResponseDto';
 import { User } from '../../database/User';
 import { GetOtherUserDto } from '../offer/DTOs/GetOtherUserDto';
 import { convertVehicleToCreateVehicleDto } from './convertToCreateVehicleDto';
+import { TripRequest } from "../../database/TripRequest";
+import { GetTripRequestResponseDto } from "../request/DTOs/GetTripRequestResponseDto";
+import { calcAge } from "./calcAge";
 
 export const convertOfferToGetOfferDto = (offer: Offer) => {
   const getOfferResponseDto: GetOfferResponseDto = new GetOfferResponseDto();
@@ -30,6 +33,30 @@ export const convertUserToOtherUser = (user: User) => {
   otherUserDto.lastName = user.lastName;
   otherUserDto.profilePicture = user.profilePicture;
   otherUserDto.entryDate = new Date(user.entryDate);
+  otherUserDto.age = calcAge(new Date(user.birthday));
+  otherUserDto.phoneNumberProvided = !!user.phoneNumber;
+  if (user.offers) {
+    otherUserDto.offers = user.offers.map((o) => convertOfferToGetOfferDto(o));
+  }
+  if (user.requestedTrips) {
+    otherUserDto.tripRequests = user.requestedTrips.map((tR)=> convertTripRequestToGetDto(tR));
+  }
 
   return otherUserDto;
 };
+
+
+export const convertTripRequestToGetDto = (tripRequest: TripRequest) => {
+  const dto = new GetTripRequestResponseDto();
+  dto.id = tripRequest.id;
+  dto.requester = convertUserToOtherUser(tripRequest.requester);
+  dto.startPlz = tripRequest.startPlz;
+  dto.endPlz = tripRequest.endPlz;
+  dto.createdAt = new Date(tripRequest.createdAt);
+  dto.description = tripRequest.description;
+  dto.cargoImg = tripRequest.cargoImg;
+  dto.startDate = tripRequest.startDate;
+  dto.seats = tripRequest.seats;
+
+  return dto;
+}
