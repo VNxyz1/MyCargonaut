1096ad6c8c12c5c9fdeda60e057e8d8f603bae72
VNxyz1 <vincenzo_n@rocketmail.com>
2024-03-09 17:57:20 +0100

    chats are now sorted by date descending

--- frontend/src/view/components/Chat-Page/Single-Chat.tsx
+++ frontend/src/view/components/Chat-Page/Single-Chat.tsx
@@ -1,11 +1,39 @@
-function SingleChat () {
+import { chatStore } from './chats-zustand.ts';
+import { Card, InputGroup } from 'react-bootstrap';
+import { Message } from '../../../interfaces/Message.ts';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
+function SingleChat () {
+  const {selectedChat} = chatStore();
 
   return (
     <>
-
+      {selectedChat.messages.map((message) => (
+        <MessageComp message={message}/>
+      ))}
+      <Form>
+        <InputGroup>
+          <Form.Control type="text" placeholder="Nachricht..."/>
+          <Button className="mainButton">
+          </Button>
+        </InputGroup>
+      </Form>
     </>
   );
 }
+
+function MessageComp( props: { message: Message }) {
+
+  return (
+    <Card>
+      <Card.Body>
+        {props.message.message}
+      </Card.Body>
+      <Card.Footer>{props.message.timestamp}</Card.Footer>
+    </Card>
+  );
+}
+
 
 export default SingleChat
--- frontend/src/view/components/Chat-Page/chats-zustand.ts
+++ frontend/src/view/components/Chat-Page/chats-zustand.ts
@@ -3,7 +3,7 @@ import { Conversation, Message } from '../../../interfaces/Message.ts';
 
 type Store = {
   chats: Conversation[]
-  selectedChatId: number
+  selectedChat: Conversation
   setChats: (conversations: Conversation[]) => void
   setSelectedChatId: (id: number) => void
   sortByDateDesc: () => void
@@ -11,11 +11,16 @@ type Store = {
 
 export const chatStore = create<Store>()((set) => ({
   chats: [],
-  selectedChatId: 0,
+  selectedChat: {
+    conversationId: 0,
+    conversationPartnerId: 0,
+    conversationPartnerName: '',
+    messages: [],
+  },
   setChats: (conversations) => set(() => ({ chats: conversations })),
-  setSelectedChatId: (id) => set(() => ({ selectedChatId: id })),
+  setSelectedChatId: (id) => set((state) => ({ selectedChat: findConversation(id, state.chats) })),
   sortByDateDesc: () => set((store) => ({ chats: sortByDateDesc(store.chats) })),
-}))
+}));
 
 function sortByDateDesc(chats: Conversation[]): Conversation[] {
   return chats.sort((a, b) => {
@@ -24,7 +29,7 @@ function sortByDateDesc(chats: Conversation[]): Conversation[] {
     const dateA = new Date(newestMessageA.timestamp);
     const dateB = new Date(newestMessageB.timestamp);
     return dateA.getTime() - dateB.getTime();
-  })
+  });
 }
 
 
@@ -32,10 +37,31 @@ function findNewestMessage(messages: Message[]): Message {
   let newest = messages[0];
   for (const m of messages) {
     const date = new Date(m.timestamp);
-    const newestDate = new Date(newest.timestamp)
-    if(date > newestDate) {
+    const newestDate = new Date(newest.timestamp);
+    if (date > newestDate) {
       newest = m;
     }
   }
   return newest;
+}
+
+function findConversation(id: number, chats: Conversation[]) {
+  if (chats.length == 0) {
+    return {
+      conversationId: 0,
+      conversationPartnerId: 0,
+      conversationPartnerName: '',
+      messages: [],
+    };
+  }
+
+  const conversation = chats.find((c) => {
+    return c.conversationId == id;
+  });
+
+  if (conversation == undefined) {
+    return chats[0];
+  }
+
+  return conversation;
 }
