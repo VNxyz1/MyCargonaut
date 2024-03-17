1e8c81f23b99f58023cf6888e52f71d4a45d03fb
Julius Maximilian Sange <julius.maximilian.sange@mni.thm.de>
2024-03-17 21:01:53 +0100

    Link to profile in Profile Diplay

--- frontend/src/view/components/Trip-Detail/ProfileDisplay.tsx
+++ frontend/src/view/components/Trip-Detail/ProfileDisplay.tsx
@@ -3,6 +3,7 @@ import {Col, Row} from "react-bootstrap";
 import placeholderImg from "../../../assets/img/user-default-placeholder.png";
 import {useEffect, useState} from "react";
 import {User} from "../../../interfaces/User.ts";
+import { Link } from "react-router-dom";
 
 function ProfileDisplay(
     props: {
@@ -30,6 +31,11 @@ function ProfileDisplay(
     return (
         <>
             <Row>
+                
+                <Link
+        to={"id" in props.user ? `/user/${props.user.id}` : `/404}`}
+        style={{ textDecoration: "none", color: "inherit" }}
+                >
                 <Col xs={"auto"}>
                     <img src={props.user.profilePicture ?
                       `${window.location.protocol}//${window.location.host}/user/profile-image/${props.user.profilePicture}` :
@@ -38,9 +44,11 @@ function ProfileDisplay(
                          alt="Profilbild"/>
                 </Col>
                 <Col>
+                
                     <h5>{props.user.firstName} {props.user.lastName}</h5>
                     Mitglied seit {entryDateDisplay}
                 </Col>
+                </Link>
             </Row>
         </>
     )
